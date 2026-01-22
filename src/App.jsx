c10e571a28056f99ccd58c5d394c95af50e877db
Miguel Angel Alvarez Ibarra <mijelito333@gmail.com>
2026-01-22 17:42:00 -0600

    Agregar página de login y opciones extra al menú

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Banner from "./componentes/Banner";
 import Cursos from "./componentes/secciones/Cursos";
 import About from "./componentes/secciones/about";
 import Home from "./componentes/secciones/home";
+import Login from "./componentes/secciones/Login";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/cursos" element={<Cursos />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </BrowserRouter>
